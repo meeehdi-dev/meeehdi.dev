568ee8ff73a39b04035990202a85a879c311d24d
Mehdi <mehdi.mhiri@gmail.com>
2026-02-23 17:06:08 +0100

    feat(resume): add maintenance page

--- app/pages/resume.vue
+++ app/pages/resume.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Resume</div>
+  <!-- FIXME: add resume -->
+  <div
+    class="flex flex-col justify-center items-center min-h-[calc(100vh-var(--ui-header-height))] pb-(--ui-header-height)"
+  >
+    <UIcon name="lucide:triangle-alert" class="text-orange-300 size-100" />
+    <h1 class="text-4xl">Coming soon...</h1>
+  </div>
 </template>
