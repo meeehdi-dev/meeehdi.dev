c87657b07b35501082e7c8492c5b5b240f56655d
Mehdi <mehdi.mhiri@gmail.com>
2026-02-24 16:25:16 +0100

    fix(turnstile): do not provide env var at build time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
     },
     public: {
       turnstile: {
-        siteKey: process.env.NUXT_PUBLIC_TURNSTILE_SITE_KEY,
+        siteKey: "",
       },
     },
   },
