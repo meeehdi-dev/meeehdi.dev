9ebe7019718bcad889a9843fda002b006a516db9
Mehdi <mehdi.mhiri@gmail.com>
2026-03-09 14:34:24 +0100

    feat(contact): add links to linkedin and freelance platforms

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,7 +1,38 @@
 <template>
-  <UPageSection title="Contact">
+  <UPageSection title="Contact" :ui="{ body: 'md:mt-8' }">
+    <template #description>
+      <USeparator class="mb-4 md:mb-8" />
+      <div class="flex gap-4 justify-center items-center">
+        <ULink href="https://www.linkedin.com/in/mehdi-mhiri/" target="_blank">
+          <img
+            src="/linkedin.svg"
+            class="h-6 object-contain hover:brightness-80 cursor-pointer"
+          />
+        </ULink>
+        <ULink href="https://www.malt.fr/profile/mehdimhiri" target="_blank">
+          <img
+            src="/malt.png"
+            class="h-6 object-contain saturate-0 brightness-1000 hover:brightness-200 cursor-pointer"
+          />
+        </ULink>
+        <ULink
+          href="https://app.comet.co/freelancer/profile/Mvbm94EbYA?params=eyJhbm9ueW1pemUiOmZhbHNlLCJkZXNpZ25Nb2RlIjpmYWxzZSwicmVhZE9ubHkiOnRydWV9"
+          target="_blank"
+        >
+          <img
+            src="/comet.svg"
+            class="h-6 object-contain hover:brightness-80 cursor-pointer"
+          />
+        </ULink>
+      </div>
+    </template>
     <template #body>
-      <USeparator icon="lucide:mail" size="sm" class="mb-4 md:mb-8" type="dashed" />
+      <USeparator
+        icon="lucide:mail"
+        size="sm"
+        class="mb-4 md:mb-8"
+        type="dashed"
+      />
       <ContactForm />
     </template>
   </UPageSection>
