d327ab9ceaff46c62001407d280a79809fc8e593
Mehdi <mehdi.mhiri@gmail.com>
2026-03-16 13:14:01 +0100

    fix(projects): tools

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -13,7 +13,7 @@ useSeoMeta({
       <HoverCard
         title="LVMH"
         description="Evolution, optimisation et maintien de différents sites de la marque."
-        :tools="['NestJS', 'NextJS', 'Prismic']"
+        :tools="['NextJS', 'NestJS', 'Prismic']"
         link="https://www.lvmh.com/"
         image="/lvmh.svg"
         light
@@ -28,7 +28,7 @@ useSeoMeta({
       <HoverCard
         title="Holo-Way"
         description="Conception et développement d'Holo-Diag, un outil de d'évalution et de suivi des actifs immatériels d'une entreprise."
-        :tools="['NextJS', 'Strapi', 'Prismic']"
+        :tools="['React', 'NodeJS', 'Express']"
         link="https://holo-way.com/"
         image="/holoway.svg"
         light
@@ -52,14 +52,14 @@ useSeoMeta({
       <HoverCard
         title="Valoref"
         description="Conception et développement d'un ERP de gestion des déchets pour la filiale de Saint-Gobain."
-        :tools="['NextJS', 'Express', 'React']"
+        :tools="['React', 'NodeJS', 'Express']"
         link="https://www.valoref.com/"
         image="/valoref.svg"
       />
       <HoverCard
         title="Canalbio"
         description="Création d'un CRM adapté à l'usage par tablette pour les commerciaux en déplacement."
-        :tools="['NextJS', 'Express', 'React']"
+        :tools="['React', 'NodeJS', 'Express']"
         link="https://www.canal.bio/"
         image="/canalbio.svg"
         light
@@ -67,14 +67,14 @@ useSeoMeta({
       <HoverCard
         title="Interson-Protac"
         description="Développement d'un outil d'aide aux professionels de l'audition lors de la fabrication de prothèses auditives."
-        :tools="['NestJS', 'Angular']"
+        :tools="['Angular', 'NodeJS']"
         link="https://www.earow-audio.fr/"
         image="/earow.png"
       />
       <HoverCard
         title="Cisco"
         description="Conception d'un outil d'aide à la vente de matériel pour les clients de la marque."
-        :tools="['NodeJS', 'AngularJS']"
+        :tools="['AngularJS', 'NodeJS']"
         link="https://www.cisco.com/"
         image="/cisco.svg"
       />
